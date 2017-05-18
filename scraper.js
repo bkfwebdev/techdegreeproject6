f0277ce289a2d1dce1f14b015a1987f936cc9b86
bkfwebdev <bryant.feld@gmail.com>
2017-05-17 23:02:03 -0400

    error log works

--- scraper.js
+++ scraper.js
@@ -41,11 +41,15 @@ return myStamps;
 
 shirtXray(appTarget,mySelector,dataTemplate)
 (function(err,myData){
+let newStamps = timeStamps();
 if (err != null && err.code == "ENOENT"){
 	console.log("There’s been a 404 error. Cannot connect to the to http://shirts4mike.com.");
 	console.dir(err);
+	fs.appendFile('./data/scraper-error.log', err.code +' '+ newStamps[0] +' '+newStamps[1], encoding='utf8', function (err) {
+    if (err) throw err; 
+	}); 
 	} else {
-let newStamps = timeStamps();
+
 for (let i = 0; i <=7; i++){myData[i].Time = newStamps[1];}
 let csv = json2csv({data:myData,fields:myFields});
 	let myFileName = newStamps[0] + '.csv';
@@ -64,38 +68,24 @@ let csv = json2csv({data:myData,fields:myFields});
 //console.log(stampTest[0],stampTest[1]);
 
 /*
-
 Instructions/grading points
-
 Scraping and Saving Data:
-
 >The scraper should get the price, title, url and image url from the product page and save this information into a CSV file.
-
 >The information should be stored in an CSV file that is named for the date it was created, e.g. 2016-11-21.csv.
-
 >Assume that the the column headers in the CSV need to be in a certain order to be correctly entered into a database. 
-
 >They should be in this order: Title, Price, ImageURL, URL, and Time
-
 >The CSV file should be saved inside the ‘data’ folder.
-
 >If your program is run twice, it should overwrite the data in the CSV file with the updated information.
-
 >If http://shirts4mike.com is down, an error message describing the issue should appear in the console.
-
 >The error should be human-friendly, such as “There’s been a 404 error. Cannot connect to the to http://shirts4mike.com.”
 To test and make sure the error message displays as expected, you can disable the wifi on your computer or device.
 */
 
 /*
 Extra Credit
-
 To get an "exceeds" rating, you can expand on the project in the following ways:
-
 2 steps
-
 1.Edit your package.json file so that your program runs when the npm start command is run.
-
 2.When an error occurs, log it to a file named scraper-error.log . 
 It should append to the bottom of the file with a time stamp and error e.g. 
 [Tue Feb 16 2016 10:02:12 GMT-0800 (PST)] <error message>
